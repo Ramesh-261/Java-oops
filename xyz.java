1fb9b2b556efc588c6bf6a638257f9289619e5de
Ramesh2611 <rp572493rameshkumar@gmail.com>
2024-07-05 07:55:00 +0530

    MethodOverriding

---
+++ xyz.java
@@ -0,0 +1,18 @@
+ class test {
+    void show()
+    {
+        System.out.println("I am Method of test class");
+    }
+}
+class xyz{
+    void show()
+    {
+        System.out.println("I am Method of xyz class");
+    }
+    public static void main(String args[]){
+       // test t=new test();
+       //   t.show();
+          xyz x=new xyz();
+          x.show();
+    }
+}
