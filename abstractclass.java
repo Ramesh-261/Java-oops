b33340dcbfe9d5fa5bbe5d7c2c7e6fd618acbc2b
Ramesh2611 <rp572493rameshkumar@gmail.com>
2024-07-14 20:10:49 +0530

    Abstract class

---
+++ abstractclass.java
@@ -0,0 +1,20 @@
+ abstract class abstractclass {
+    abstract void start();
+}
+class car extends abstractclass{
+    void start(){
+        System.out.println("start with key");
+    }
+}
+class scooter extends abstractclass{
+    void start(){
+        System.out.println("start with kick");
+    }
+    public static void main(String args[]){
+        //abstractclass v=new abstractclass(); //not create object due to abstract
+        car c=new car();
+        c.start();
+        scooter s=new scooter();
+        s.start();
+    }
+}
