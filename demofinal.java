0b34bba7e51e3180395307df11efbf5d7a31cd07
Ramesh2611 <rp572493rameshkumar@gmail.com>
2024-07-23 12:26:55 +0530

    finalkeyword

---
+++ demofinal.java
@@ -0,0 +1,8 @@
+class demofinal {
+    public static void main(String[] args) {
+     final int i = 10;
+      i = i + 20;
+      System.out.println(i);
+    }
+  }
+  
--- final.java
+++
@@ -1,9 +0,0 @@
-class finals
-{
-  public static void main(String args[])
-    {
-      int i=10;
-      i=i+20;
-      System.out.println(i);
-     }
- }   
