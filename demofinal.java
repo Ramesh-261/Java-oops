1f5b05a8224e268c7082eafd3e1f5e46b04617ba
Ramesh2611 <rp572493rameshkumar@gmail.com>
2024-07-23 12:29:55 +0530

    finalkeyword

--- demofinal.java
+++ demofinal.java
@@ -1,6 +1,6 @@
 class demofinal {
     public static void main(String[] args) {
-     final int i = 10;
+     final int i = 10; //can't assign again
       i = i + 20;
       System.out.println(i);
     }
